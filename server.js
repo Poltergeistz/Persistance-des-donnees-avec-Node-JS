9bb3cceeb5faaea0e82518dc6fe68a62fcf7e60c
david <david.journo.31@gmail.com>
2018-06-18 14:46:59 +0200

    listen port

--- server.js
+++ server.js
@@ -8,4 +8,11 @@ let app = express();
 app.set('view engine', 'ejs');
 
 // Routes
+app.get('/', function (req,res){
+    res.send('test');
+})
 
+// Listen
+app.listen(8080, function (req,res){
+    console.log('Server Online')
+})
