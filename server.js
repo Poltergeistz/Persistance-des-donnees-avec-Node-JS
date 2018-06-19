7faa3111ce2aa908aa050a5d2ed58c5efd5ee144
philippe <huhmiel@free.fr>
2018-06-19 11:38:57 +0200

    created class posts

--- server.js
+++ server.js
@@ -7,16 +7,25 @@ let app = express();
 app.set('view engine', 'ejs');
 
 // Routes
-app.get('/', function (req,res){
+app.get('/', function (req, res) {
     res.send('test');
 })
 
 // Listen
-app.listen(8080, function (req,res){
+app.listen(8080, function (req, res) {
     console.log('Server Online')
 })
 
-
+//Post class
+class Post {
+    constructor(title, post, date, author) {
+        this.title = title;
+        this.post = post;
+        this.date = date;
+        this.author = author;
+    }
+}
+var post1 = new Post('test', 'je suis du texte', '12/12/05', 'moi');
 
 const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
@@ -24,8 +33,6 @@ const assert = require('assert');
 (async function () {
     // Connection URL
     const url = 'mongodb://Phil76:SimplonERN76@ds161740.mlab.com:61740/mongo_blog';
-    // Database Name
-    //const dbName = 'myproject';
     let client;
 
     try {
@@ -39,42 +46,34 @@ const assert = require('assert');
     }
 
     if (client) {
-        //client.db.show
-        //console.log(client.db('mongo_blog'))
         const dbName = 'mongo_blog'
         const db = client.db(dbName);
-        console.log("test: ", db.collection.s)
+
 
         //ajouter un document
-        var objNew = {
-            name: "GLaDOS",
-            game: "Portal"
-        };
-
-        /*db.collection("posts").insert(objNew, null, function (error, results) {
+        db.collection("posts").insert(post1, null, function (error, results) {
             if (error) throw error;
 
             console.log("Le document a bien été inséré");
-        });*/
+            //client.close();
+        });
         //afficher un document
         db.collection("posts").find().toArray(function (error, results) {
             if (error) throw error;
-            console.log("results: ", results)
+            //console.log("results: ", results)
             console.log("table posts:")
             results.forEach(function (obj, i) {
                 console.log(obj)
-                /*db.collection("posts").remove(obj._id, null, function (error, result) {
-                    if (error) throw error;
-                });*/
-                //console.lo(obj)
             });
-
+            client.close();
         });
         //supprimer tous les documents
-        db.collection("posts").remove( null, function (error, result) {
+        /*db.collection("posts").remove(null, function (error, result) {
             if (error) throw error;
         });
-        console.log("all posts removed")
-        client.close();
+        console.log("all posts removed")*/
+        //client.close();
+        
     }
+    //client.close();
 })();
