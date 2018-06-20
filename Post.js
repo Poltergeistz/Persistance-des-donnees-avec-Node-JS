3dccd868257ffb3b14c8276ea19e18379c746159
philippe <huhmiel@free.fr>
2018-06-20 08:01:19 +0200

    added function search and delete posts

---
+++ Post.js
@@ -0,0 +1,8 @@
+function Post (title, post, date, author) {
+    this.title = title;
+    this.post = post;
+    this.date = date;
+    this.author = author;
+}
+module.exports = Post;
+
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 // Modules
 let express = require('express');
 let ejs = require('ejs');
-
+let Post = require('./Post');
 // Set express
 let app = express();
 app.set('view engine', 'ejs');
@@ -16,87 +16,45 @@ app.listen(8080, function (req, res) {
     console.log('Server Online')
 })
 
-//Post class
-class Post {
-    constructor(title, post, date, author) {
-        this.title = title;
-        this.post = post;
-        this.date = date;
-        this.author = author;
-    }
-}
-var post1 = new Post('test', 'je suis du texte', '12/12/05', 'moi');
-
 const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
 
-(async function () {
-    // Connection URL
-    const url = 'mongodb://admin:HG13admin@ds161740.mlab.com:61740/mongo_blog';
-    let client;
-
-    try {
-        // Use connect method to connect to the Server
-        client = await MongoClient.connect(url);
-
-        const db = client.db('dbName');
-
-    } catch (err) {
-        console.log(err.stack);
-    }
-
-    if (client) {
-        const dbName = 'mongo_blog'
-        const db = client.db(dbName);
 
 
-        //ajouter un document
-        db.collection("posts").insert(post1, null, function (error, results) {
-            if (error) throw error;
-
-            console.log("Le document a bien été inséré");
-            //client.close();
-        });
-        //afficher un document
-        db.collection("posts").find().toArray(function (error, results) {
-            if (error) throw error;
-            //console.log("results: ", results)
-            console.log("table posts:")
-            results.forEach(function (obj, i) {
-                
-                    console.log(obj, i)
-                
-            });
-            client.close();
-        });
 
 
-        //recherche par id
-        /*var MongoObjectID = require("mongodb").ObjectID; // Il nous faut ObjectID
-        var idToFind = "5b28e8e91309d90bb879f71c"; // Identifiant, sous forme de texte
-        var objToFind = {
-            _id: new MongoObjectID(idToFind)
-        };
-        db.collection("posts").findOne(objToFind, function (error, result) {
-            if (error) throw error;
-
-            console.log(result);
-        });*/
 
 
-        //supprimer tous les documents
-        /*db.collection("posts").remove(null, function (error, result) {
-            if (error) throw error;
-        });
-        console.log("all posts removed")*/
-        //client.close();
 
-    }
-    //client.close();
-})();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 function newPost(title, post, date, author) {
-    var post = new Post(title, post, date, author);
+    var postUser = new Post(title, post, date, author);
+    //console.log("newpot", postUser)
     (async function () {
         const url = 'mongodb://admin:HG13admin@ds161740.mlab.com:61740/mongo_blog';
         let client;
@@ -111,7 +69,7 @@ function newPost(title, post, date, author) {
             const dbName = 'mongo_blog'
             const db = client.db(dbName);
             //ajouter un document
-            db.collection("posts").insert(post, null, function (error, results) {
+            db.collection("posts").insert(postUser, null, function (error, results) {
                 if (error) throw error;
                 console.log("Le document a bien été inséré");
                 client.close();
@@ -120,4 +78,102 @@ function newPost(title, post, date, author) {
     })();
 }
 
-newPost('tutoooo', 'je suis encore du texte', '12/06/05', 'bob')
+function showLastPosts() {
+    (async function () {
+        const url = 'mongodb://admin:HG13admin@ds161740.mlab.com:61740/mongo_blog';
+        let client;
+        try {
+            // Use connect method to connect to the Server
+            client = await MongoClient.connect(url);
+            const db = client.db('dbName');
+        } catch (err) {
+            console.log(err.stack);
+        }
+        if (client) {
+            const dbName = 'mongo_blog'
+            const db = client.db(dbName);
+            //afficher un document
+            db.collection("posts").find().toArray(function (error, results) {
+                if (error) throw error;
+                //console.log("results: ", results)
+                console.log("last 10 posts:")
+                for (var i = results.length - 1; i > results.length - 11; i--) {
+                    console.log(results[i], i)
+                }
+                client.close();
+            });
+        }
+    })();
+}
+
+function searchPost(id) {
+    (async function () {
+        const url = 'mongodb://admin:HG13admin@ds161740.mlab.com:61740/mongo_blog';
+        let client;
+        try {
+            // Use connect method to connect to the Server
+            client = await MongoClient.connect(url);
+            const db = client.db('dbName');
+        } catch (err) {
+            console.log(err.stack);
+        }
+        if (client) {
+            const dbName = 'mongo_blog'
+            const db = client.db(dbName);
+            //rechercher un document par id
+            var MongoObjectID = require("mongodb").ObjectID; // Il nous faut ObjectID
+            var idToFind = id; // Identifiant, sous forme de texte
+            var objToFind = {
+                _id: new MongoObjectID(idToFind)
+            };
+            db.collection("posts").findOne(objToFind, function (error, result) {
+                if (error) {
+                    throw error;
+                }
+                if (result == null) {
+                    console.log("search: document not found")
+                } else {
+                    console.log("search: ", result);
+                }
+            });
+        }
+    })();
+}
+
+function removePost(id) {
+    console.log("id: ", id);
+    (async function () {
+        const url = 'mongodb://admin:HG13admin@ds161740.mlab.com:61740/mongo_blog';
+        let client;
+        try {
+            // Use connect method to connect to the Server
+            client = await MongoClient.connect(url);
+            const db = client.db('dbName');
+        } catch (err) {
+            console.log(err.stack);
+        }
+        if (client) {
+            const dbName = 'mongo_blog'
+            const db = client.db(dbName);
+            //supprimer un documents
+            var MongoObjectID = require("mongodb").ObjectID; // Il nous faut ObjectID
+            var idToFind = id; // Identifiant, sous forme de texte
+            var objToFind = {
+                _id: new MongoObjectID(idToFind)
+            }; // Objet qui va nous servir pour effectuer la recherche
+            db.collection("posts").remove(objToFind, null, function (error, result) {
+                if (error) {
+                    throw error;
+                } else if (id) {
+                    console.log("document " + id + " removed") 
+                } else {
+                    console.log("document " + id + " does not exist, nothing to remove")
+                }
+            });
+        };
+    })();
+}
+//newPost('tutoooo', 'je suis encore du texte', '12/06/05', 'billou');
+showLastPosts();
+//searchPost('5b2918b8c47b5811c031fbe3')
+//removePost('5b291860ea629f1182563fee')
